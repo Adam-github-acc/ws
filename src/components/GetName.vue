8d567ebd0304adace9783cfc7a6f0063b1bdefb3
Adam Hallberg <adamdavidhallberg@gmail.com>
2023-02-14 17:38:51 +0100

    feat:add input field

--- src/components/GetName.vue
+++ src/components/GetName.vue
@@ -25,6 +25,14 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="get-name">
+    <input placeholder="Letter" type="text" v-model="myInput" maxlength="1" />
+    <button @click="getRandomName(myInput)">Get Name</button>
+  </div>
+  <div v-if="randomName">Name is: {{ randomName }}</div>
+  <div v-else-if="error">Something went wrong try again</div>
+  <div v-else>Enter the letter you want the name to start with</div>
+</template>
 
 <style scoped></style>
