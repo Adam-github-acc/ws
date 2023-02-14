067da98f50d4141f9e8a49df498b29dcb8123a4c
Adam Hallberg <adamdavidhallberg@gmail.com>
2023-02-14 17:34:15 +0100

    feat:add getRandomName method w/data

--- src/components/GetName.vue
+++ src/components/GetName.vue
@@ -1,4 +1,29 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      randomName: '',
+      error: false,
+    };
+  },
+  methods: {
+    async getRandomName(letter) {
+      try {
+        this.error = false;
+        const res = await fetch('https://randomuser.me/api/');
+        const json = await res.json();
+        const name = json.results[0].name.first;
+        if (name[0].toLowerCase() === letter.toLowerCase())
+          this.randomName = name;
+        else this.getRandomName(letter);
+      } catch (error) {
+        console.log(error);
+        this.error = true;
+      }
+    },
+  },
+};
+</script>
 
 <template></template>
 
